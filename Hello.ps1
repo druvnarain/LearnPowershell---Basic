515e1c8e648f325f96aa8f0c69d4ec8bd2afde24
druvnarain <50425550+druvnarain@users.noreply.github.com>
2021-12-01 20:07:52 -0500

    Launch.json for vscode debug, comments

--- Hello.ps1
+++ Hello.ps1
@@ -1,2 +1,32 @@
+# This is a single line comment
+
+<#
+Multi line comment
+#>
+
+# Comment-Based Help
+<#
+  .SYNOPSIS
+   Says hello to anyone who runs this script.
+  .DESCRIPTION
+   The Hello.ps1 script prints our Hello how are you to the screen.
+  .PARAMETER Name
+   Specifies the name of the person running the script.
+  .INPUTS
+   None. You cannot pipe objects to Hello.ps1.
+  .OUTPUTS
+   Writes two string objects "Hello" and "How are you?" to the console.
+  .EXAMPLE
+   C:\PS> .\Hello.ps1
+  .EXAMPLE
+   C:\PS> .\Hello.ps1 -Name Ian
+#>
+
 Write-Host "Hello"
 Write-Host "How are you"
+
+# Testing variable assignment with debugging in VSCode
+$number = 1
+
+Write-Host "The number is: " $number
+
